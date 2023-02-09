4af6505c849f6d732c2849d88ab42d33bb8861ed
Aleksander <asava@sibmail.com>
2023-02-09 18:46:58 +0700

    закончили, раб REDUX

--- src/app/App.js
+++ src/app/App.js
@@ -5,8 +5,6 @@ import Users from './layouts/users'
 import Login from './layouts/login'
 import Main from './layouts/main'
 import NavBar from './components/ui/navBar'
-import { QualitiesProvider } from './hooks/useQualities'
-import AuthProvider from './hooks/useAuth'
 import ProtectedRoute from './components/common/protectedRoute'
 import LogOut from './layouts/logOut'
 import AppLoader from './components/ui/hoc/appLoader'
@@ -15,21 +13,17 @@ function App() {
     return (
         <div>
             <AppLoader>
-                <AuthProvider>
-                    <NavBar />
-                    <QualitiesProvider>
-                        <Switch>
-                            <ProtectedRoute
-                                path="/users/:userId?/:edit?"
-                                component={Users}
-                            />
-                            <Route path="/login/:type?" component={Login} />
-                            <Route path="/logout" component={LogOut} />
-                            <Route path="/" exact component={Main} />
-                            <Redirect to="/" />
-                        </Switch>
-                    </QualitiesProvider>
-                </AuthProvider>
+                <NavBar />
+                <Switch>
+                    <ProtectedRoute
+                        path="/users/:userId?/:edit?"
+                        component={Users}
+                    />
+                    <Route path="/login/:type?" component={Login} />
+                    <Route path="/logout" component={LogOut} />
+                    <Route path="/" exact component={Main} />
+                    <Redirect to="/" />
+                </Switch>
             </AppLoader>
 
             <ToastContainer />
