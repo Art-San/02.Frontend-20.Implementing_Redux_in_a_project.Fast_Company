a80e050d907a9fc98a6a2da449d4da24ee58e450
Aleksander <asava@sibmail.com>
2023-06-19 15:45:16 +0700

    // Создаем Store

--- src/app/store/createStore.js
+++ src/app/store/createStore.js
@@ -1,6 +1,6 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-
-const rootReducer = combineReducers({}) // Создаем Store
+// Создаем Store
+const rootReducer = combineReducers({})
 
 export function createStore() {
     return configureStore({
