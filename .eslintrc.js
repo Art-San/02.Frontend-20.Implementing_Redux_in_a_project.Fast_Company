6f12a08a2eb2cfee07e9af81104f7ccb76c93c65
Aleksander <asava@sibmail.com>
2023-06-19 22:27:12 +0700

    ok

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     plugins: ['react'],
     rules: {
-        indent: ['error', 4],
+        indent: [0, 4],
         semi: ['error', 'never'],
         'space-before-function-paren': [
             'error',
