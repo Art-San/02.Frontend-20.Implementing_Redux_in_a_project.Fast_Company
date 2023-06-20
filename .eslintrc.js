1f7c0331509f916b69f831e09b007f92a3c7e416
Aleksander <asava@sibmail.com>
2023-06-20 19:13:28 +0700

    0k

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     plugins: ['react'],
     rules: {
-        indent: ['error', 4],
+        indent: [0, 4],
         semi: ['error', 'never'],
         'space-before-function-paren': [
             'error',
@@ -27,35 +27,3 @@ module.exports = {
         ]
     }
 }
-
-// module.exports = {
-//     env: {
-//         browser: true,
-//         es2021: true
-//     },
-//     extends: ["plugin:react/recommended", "standard"],
-//     parserOptions: {
-//         ecmaFeatures: {
-//             jsx: true
-//         },
-//         ecmaVersion: 12,
-//         sourceType: "module"
-//     },
-//     plugins: ["react"],
-//     rules: {
-//         semi: [2, "always"],
-//         indent: [0, 4],
-//         "space-before-function-paren": [
-//             "error",
-//             { anonymous: "always", named: "never" }
-//         ],
-//         "multiline-ternary": ["off"],
-//         quotes: [
-//             "error",
-//             "double",
-//             {
-//                 allowTemplateLiterals: true
-//             }
-//         ]
-//     }
-// };
